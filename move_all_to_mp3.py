8bc5df3f2c62c479b3f40ba2321191b09c8a7376
Bea Nance <bwnance@gmail.com>
2025-06-15 14:11:36 -0400

    add some logic to find broken stuff

---
+++ move_all_to_mp3.py
@@ -0,0 +1,61 @@
+import os
+from pyrekordbox import Rekordbox6Database
+import time
+import subprocess
+
+try:
+    db = Rekordbox6Database()
+except:
+    print("getting key")
+    subprocess.run(["python -m pyrekordbox download-key"], shell=True)
+    print("run me again!")
+    exit(1)
+deviceid = db.get_device()[0].ID
+content = db.get_content()
+
+songs = [
+    c
+    for c in content
+    if c.Title is not None
+    and c.FolderPath is not None
+    and "soundcloud:" not in c.FolderPath
+]
+for song in songs:
+    if "Users/Shared" in song.FolderPath:
+        continue
+    org_path = song.OrgFolderPath
+    if org_path is not None:
+        song.FolderPath = song.OrgFolderPath
+    else:
+        mp3_name = song.FolderPath.split("/")[-1]
+        new_path = f"/Users/Shared/DJ_Tracks/{mp3_name}"
+        song.FolderPath = new_path
+    song.DeviceID = deviceid
+    if ".mp3" not in song.FolderPath:
+        song_pieces = song.FolderPath.split(".")
+        song[-1] = "mp3"
+        new_path = ".".join(song_pieces)
+        if not os.path.exists(new_path):
+            print(f"non-mp3 doesn't exist: {new_path}")
+            print(f"old path: {song.FolderPath}")
+success = False
+try:
+    db.commit()
+    success = True
+except:
+    print("rekordbox is probably running, killing it and trying again")
+    subprocess.call(["osascript -e 'quit app \"rekordbox\"'"], shell=True)
+    # sleep for a bit to ensure the app has time to close
+    for i in range(10):
+        print(f"Waiting for rekordbox to close...{i}")
+        time.sleep(1)
+        try:
+            db.commit()
+            success = True
+            break
+        except:
+            continue
+if success:
+    print("rewrote all song paths!")
+else:
+    print("failed to close rekordbox! please retry.")
